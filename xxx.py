409b635e1baa4fb286ddc91b1c0a19f7f9c5e925
小马三愆 <31155461+ULTRAMANSE@users.noreply.github.com>
2019-01-14 17:41:28 +0800

    Delete xxx.py

--- xxx.py
+++
@@ -1,7 +0,0 @@
-from time import localtime,strftime
-
-print(strftime("%M:%S", localtime(154)))
-print(strftime("%M:%S", localtime(555)))
-print(strftime("%M:%S", localtime(969)))
-print(strftime("%M:%S", localtime(1371)))
-print(strftime("%M:%S", localtime(1774)))
